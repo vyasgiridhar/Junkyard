60ac45e4d9ff81cebb6a233891fc07f088487c8d
vyasgiridhar <vyasgiridhar27@gmail.com>
2016-05-16 19:09:26 +0530

    Added Locating service

--- app/src/main/java/creative/nurseme/MainActivity.java
+++ app/src/main/java/creative/nurseme/MainActivity.java
@@ -1,17 +1,30 @@
 package creative.nurseme;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MainActivity extends FragmentActivity
+        implements OnMapReadyCallback {
+    private GoogleMap Map;
+    private MapFragment mMapFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +33,30 @@ public class MainActivity extends AppCompatActivity {
         //setSupportActionBar(toolbar);
 
 
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+        //  Intent intent = new Intent(this, LoginActivity.class);
+        //  startActivity(intent);
+        int x= 0 ;
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
     }
+    @Override
+    public void onMapReady(GoogleMap map) {
+//DO WHATEVER YOU WANT WITH GOOGLEMAP
+        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            map.setMyLocationEnabled(true);
+        } else {
+            Toast.makeText(this, "You have to accept to enjoy all app's services!", Toast.LENGTH_LONG).show();
+        }
+        map.setTrafficEnabled(true);
+        map.setIndoorEnabled(true);
+        map.setBuildingsEnabled(true);
+        map.getUiSettings().setZoomControlsEnabled(true);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
